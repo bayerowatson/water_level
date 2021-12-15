e3aaeca3621d0664283cef694289592f7ba09f07
bayer <bayerowatson@gmail.com>
2021-12-15 12:52:42 -0500

    working on waterData class

--- emailer.js
+++ emailer.js
@@ -1,47 +1,56 @@
-import * as waterData from './waterData.js';
-import nodemailer from 'nodemailer'
+import * as waterService from './waterService.js';
+import nodemailer from 'nodemailer';
 
 
-export default class Emailer {
-
+export default class waterData {
     constructor() {
-        this.driesData = await this.waterData.getDries();
-        this.driesLevel = this.driesData.data.value.timeSeries[0].values[0].value[0];
-        this.gauleyData = await this.waterData.getGauley();
-        this.gauleyLevel = this.gauleyData.data.value.timeSeries[2].values[0].value[0];
+        this.transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              type: 'OAuth2',
+              user: process.env.MAIL_USERNAME,
+              pass: process.env.MAIL_PASSWORD,
+              clientId: process.env.OAUTH_CLIENTID,
+              clientSecret: process.env.OAUTH_CLIENT_SECRET,
+              refreshToken: process.env.OAUTH_REFRESH_TOKEN
+            }
+          }); 
+          this.mailOptions = {
+            from: 'wvriversurfing@gmail.com',
+            to: 'bayerowatson@gmail.com',
+            subject: 'Today\'s water level',
+            text: '' 
+            };   
+    } 
+
+    async updateLevels() {
+        let driesData = await waterService.getDries();
+        let driesLevel = driesData.data.value.timeSeries[0].values[0].value[0];
+        let gauleyData = await waterService.getGauley();
+        let gauleyLevel = gauleyData.data.value.timeSeries[2].values[0].value[0];
         console.log('Dries level:', driesLevel);
         console.log('Gauley level:', gauleyLevel);
+        this.mailOptions.text = `
+            New River Dries: ${this.driesLevel.value} ft.
+            Last updated: ${this.driesLevel.dateTime}
+
+            Gauley River Below Summersville Dam: ${this.gauleyLevel.value} ft.
+            Last updated: ${this.gauleyLevel.dateTime}` 
     }
 
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          type: 'OAuth2',
-          user: process.env.MAIL_USERNAME,
-          pass: process.env.MAIL_PASSWORD,
-          clientId: process.env.OAUTH_CLIENTID,
-          clientSecret: process.env.OAUTH_CLIENT_SECRET,
-          refreshToken: process.env.OAUTH_REFRESH_TOKEN
-        }
-      });    
-
-    let mailOptions = {
-        from: 'wvriversurfing@gmail.com',
-        to: 'bayerowatson@gmail.com',
-        subject: 'Today\'s water level',
-        text: `
-            New River Dries: ${driesLevel.value} ft.
-            Last updated: ${driesLevel.dateTime}
-    
-            Gauley River Below Summersville Dam: ${gauleyLevel.value} ft.
-            Last updated: ${gauleyLevel.dateTime}` 
-    };
-
-    transporter.sendMail(mailOptions, function(err, data) {
-        if (err) {
-            console.log("Error " + err);
-        } else {
-            console.log("Email sent successfully");
-        }
-    });
+    sendEmail() {
+        this.transporter.sendMail(this.mailOptions, function(err, data) {
+            if (err) {
+                console.log("Error " + err);
+            } else {
+                console.log("Email sent successfully");
+            }
+        });
+    }
 };
+
+let water = new waterData();
+
+water.updateLevels();
+
+water.sendEmail();
--- schedule.js
+++ schedule.js
@@ -1,23 +1,19 @@
-import schedule from 'node-schedule';
-import express from 'express'; 
+import nodemailer from 'nodemailer';
 
-const app = express()
-const port = 3000
-
-
-app.listen(port, () => {
-  console.log(`scheduler is listening at http://localhost:${port}`)
-})
-
-let x = 1;
-
-let y = {number: x}
-
-x = 2;
-
-console.log(y);
-
-const job = schedule.scheduleJob('3 10 * * *', function(){
-    x = 2;
-    console.log(y);
-});
+transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD,
+    clientId: process.env.OAUTH_CLIENTID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN
+  }
+}); 
+mailOptions = {
+  from: 'wvriversurfing@gmail.com',
+  to: 'bayerowatson@gmail.com',
+  subject: 'Today\'s water level',
+  text: 'this is a test' 
+  };   
