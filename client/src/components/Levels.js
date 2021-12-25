baa729479172f056c05905551474a19f55732661
bayer <bayerowatson@gmail.com>
2021-12-24 22:53:42 -0500

    set up temp levels page

--- client/src/components/Levels.js
+++ client/src/components/Levels.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Levels = () => {
+    const [levels, setLevels] = useState("loading...");
+
+    const getLevels = () => {
+        axios
+            .get(`http://localhost:5000/levels`)
+            .then(res => {
+                console.log(res.data);
+                setLevels(res.data);
+            })
+            .catch(err => console.log(err));
+    }
+
+    useEffect(()=> {
+        getLevels();
+    }, [])
+    
     return ( 
         <div>
             <div className="h1">
                 Levels page
             </div>
+            <div className="lead">{JSON.stringify(levels)}</div>
         </div>
     
      );
