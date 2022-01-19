a5ca8feb8ce802e2246d3beced41e9e86d5b534d
bayer <bayerowatson@gmail.com>
2022-01-19 14:40:29 -0500

    fix padding on navbar links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -10,15 +10,31 @@ const Navbar = () => {
    
       <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark shadow-lg">
         <div className="container-fluid">
-          <HashLink className="navbar-brand" to="/#top"><span data-bs-toggle="collapse" data-bs-target="#navbarOptions"><i className="bi bi-tsunami"></i>WV River Surfing</span></ HashLink>
+          <HashLink className="navbar-brand" to="/#top">
+            <div data-bs-toggle="collapse" data-bs-target="#navbarOptions">
+              <i className="bi bi-tsunami" /> WV River Surfing
+            </div>
+          </ HashLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarOptions" aria-controls="navbarOptions" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+            <i className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarOptions">
             <div className="navbar-nav">
-              <HashLink className="nav-link" to="/#levels"><span data-bs-toggle="collapse" data-bs-target="#navbarOptions">Current Levels</span></HashLink>
-              <HashLink className="nav-link" to="/#subscribe"><span data-bs-toggle="collapse" data-bs-target="#navbarOptions">Subscribe</span></HashLink>
-              <Link className="nav-link" to="/unsubscribe"><span data-bs-toggle="collapse" data-bs-target="#navbarOptions">Unsubscribe</span></Link>
+              <HashLink className="nav-link p-0" to="/#levels">
+                  <div className="p-1" data-bs-toggle="collapse" data-bs-target="#navbarOptions">
+                    Current Levels
+                  </div>
+              </HashLink>
+              <HashLink className="nav-link p-0" to="/#subscribe">
+                <div className="p-1" data-bs-toggle="collapse" data-bs-target="#navbarOptions">
+                  Subscribe
+                </div>
+              </HashLink>
+              <Link className="nav-link p-0" to="/unsubscribe">
+                <div className="p-1" data-bs-toggle="collapse" data-bs-target="#navbarOptions">
+                  Unsubscribe
+                </div>
+              </Link>
             </div>
           </div>
         </div>
