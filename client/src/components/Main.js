a21c1ce003ea0dcd31eb4efb999e7c6d313e5386
bayer <bayerowatson@gmail.com>
2022-01-19 15:19:15 -0500

    fix main title font size

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -18,7 +18,7 @@ const Main = () => {
                 style={bgStyle} 
                 className="text-center d-flex flex-column justify-content-center text-white"
             >
-                <h1 className="display-2 fw-bold">WV River Surfing</h1>
+                <h1 className="display-3 fw-bold">WV River Surfing</h1>
                 <p className="lead">Providing email updates for river surfing water levels</p>
                 <div>
                     <a href="#levels" className="btn btn-primary m-1">Current Water Levels</a>
